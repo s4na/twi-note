c73fc14a8431a1972933b7f649ff74e6705bf5e8
s4na <appletea.umauma@gmail.com>
2019-11-12 17:26:02 +0900

    ノート機能の追加

---
+++ db/migrate/20191112055711_create_notes.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class CreateNotes < ActiveRecord::Migration[6.0]
+  def change
+    create_table :notes do |t|
+      t.string :title
+      t.text :body
+      t.timestamps
+    end
+  end
+end
