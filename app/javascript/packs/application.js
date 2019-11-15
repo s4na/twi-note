6c4531545940c4703a4cc939f15de4e5afc818e4
s4na <appletea.umauma@gmail.com>
2019-11-15 19:13:19 +0900

    remove turbolinks（commitし忘れ）

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,7 @@
 // that code so it'll be compiled.
 
 require("@rails/ujs").start()
-require("turbolinks").start()
+// require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
@@ -17,3 +17,4 @@ require("channels")
 // const imagePath = (name) => images(name, true)
 
 require("../tweets.js")
+
