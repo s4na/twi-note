7bacbb75ec9fb7a883cf2a50639c515a39a73386
s4na <appletea.umauma@gmail.com>
2019-11-16 18:41:31 +0900

    change tweets search query

--- app/controllers/api/tweets_controller.rb
+++ app/controllers/api/tweets_controller.rb
@@ -7,9 +7,9 @@ class Api::TweetsController < ApplicationController
 
   def index
     query = params[:query]
-    # start_datetime = Time.strptime(params[:start_datetime], "%Y-%m-%d %H:%M").to_time
+    start_datetime = Time.strptime(params[:start_datetime], "%Y-%m-%d %H:%M").to_time
     end_datetime = Time.strptime(params[:end_datetime], "%Y-%m-%d %H:%M").to_time
-    end_date = I18n.l(end_datetime, format: :ymd)
+    end_date = I18n.l(end_datetime, format: :ymd_hy)
 
     client = Twitter::REST::Client.new(
       consumer_key: ENV["CONSUMER_KEY"],
@@ -22,9 +22,14 @@ class Api::TweetsController < ApplicationController
       result_type: "recent",
       until: end_date,
       exclude: "retweets",
-      since_id: since_id)
+      since_id: since_id).to_h
 
-    @tweets = result_tweets.to_h
+    result_tweets[:statuses].select! do |tweet|
+      tweet_datetime = Time.parse(tweet[:created_at].to_s)
+      (start_datetime < tweet_datetime) && (end_datetime > tweet_datetime)
+    end
+
+    @tweets = result_tweets
     @tweets = @tweets[:statuses]
   end
 end
