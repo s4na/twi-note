0431fce65f26fe32c91f41fbcddfec95c2e8a871
s4na <appletea.umauma@gmail.com>
2019-11-21 15:41:23 +0900

    Change test to see Tweet text

--- test/system/api/tweets_test.rb
+++ test/system/api/tweets_test.rb
@@ -13,6 +13,6 @@ class TweetsTest < ApplicationSystemTestCase
     click_on "追加"
 
     click_on "検索"
-    assert_text '"id": 1195372469411958800'
+    assert_text "Markdown、初めて見たときはフォントのせいもあってか、「ゲジゲジみたいだな」って思った記憶"
   end
 end
