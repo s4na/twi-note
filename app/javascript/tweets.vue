87446dd6894667e943e6b54da13d0aefb3dcf1d8
s4na <appletea.umauma@gmail.com>
2019-11-21 15:12:59 +0900

    add Tweet Vue.Draggable

--- app/javascript/tweets.vue
+++ app/javascript/tweets.vue
@@ -1,28 +1,47 @@
 <template lang="pug">
   .tweets
-    label
-      | query
-    input(ref="query" type="text" value="s4na_penguin")
-    label
-      | tart_datetime
-    input(ref="start_datetime" type="text" value="2019-11-15 23:59")
-    label
-      | end_datetime
-    input(ref="end_datetime" type="text" value="2019-11-16 23:59")
+    .tweet(style="max-width: 400px;")
+      .search-tweets
+        p
+          | Tweets
+        draggable(class="list-group" :list="result_tweets" group="people" @change="log")
+          .list-group-item(v-for="(element, index) in result_tweets" :key="element.name")
+            | {{ element.text }} {{ index }}
+    .tweet(style="max-width: 400px;")
+      .note-tweets
+        p
+          | Notes
+        draggable(class="list-group" :list="note_tweets" group="people" @change="log")
+          .list-group-item(v-for="(element, index) in note_tweets" :key="element.name")
+            | {{ element.text }} {{ index }}
+
+    | 検索欄
     .search
-      button(@click="searchTweets")
-        | 検索
-    .results
-      | {{ this.tweets }}
+      label
+        | query
+      input(ref="query" type="text" value="s4na_penguin")
+      label
+        | tart_datetime
+      input(ref="start_datetime" type="text" value="2019-11-15 23:59")
+      label
+        | end_datetime
+      input(ref="end_datetime" type="text" value="2019-11-19 23:59")
+      .search
+        button(@click="searchTweets")
+          | 検索
 </template>
 <script>
+import Draggable from 'vuedraggable'
+
 export default {
   props: {},
   components: {
+    'draggable': Draggable
   },
   data: function () {
     return {
-      tweets: []
+      result_tweets: [],
+      note_tweets: []
     }
   },
   methods: {
@@ -31,7 +50,7 @@ export default {
       return meta ? meta.getAttribute('content') : ''
     },
     searchTweets: function() {
-      this.tweets = [];
+      this.result_tweets = [];
       const query = encodeURI(this.$refs.query.value);
       const start_datetime = encodeURI(this.$refs.start_datetime.value);
       const end_datetime = encodeURI(this.$refs.end_datetime.value);
@@ -48,7 +67,7 @@ export default {
       })
         .then(response => { return response.json(); })
         .then(json=> {
-          json.forEach(c => { this.tweets.push(c); });
+          json.forEach(c => { this.result_tweets.push(c); });
         })
         .catch(error => { console.warn('Failed to parsing', error); })
     }
