0d3377a4e444002dd197778c4c10ef3c092974e5
s4na <appletea.umauma@gmail.com>
2019-11-28 15:49:31 +0900

    add save as markdown

--- app/javascript/tweets_markdown.vue
+++ app/javascript/tweets_markdown.vue
@@ -3,7 +3,7 @@
     label
       | 本文
     textarea(name="note[body]" id="note_body")
-      | {{ valueString }}
+      | {{ to_markdown(valueString) }}
 </template>
 <script>
 const props = {
@@ -23,6 +23,16 @@ export default {
     valueString() {
       return JSON.stringify(this.value, null, 2);
     }
+  },
+  methods: {
+    to_markdown (tweets) {
+      var result = "";
+      tweets = JSON.parse(tweets);
+      for(var tweet of tweets){
+        result = result + `> [${tweet["text"]}](https://twitter.com/${tweet["user"]["screen_name"]}/status/${tweet["id_str"]}?ref_src=twsrc%5Etfw)`.replace(/\r?\n/g, ' ') + "\n\n";
+      }
+      return result;
+    },
   }
 };
 </script>
