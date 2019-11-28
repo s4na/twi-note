ce52b188c0008ed61b690fcbe9bc7d74c66889c0
s4na <appletea.umauma@gmail.com>
2019-11-28 14:21:54 +0900

    add tweet save

--- app/javascript/tweets.vue
+++ app/javascript/tweets.vue
@@ -1,59 +1,60 @@
 <template lang="pug">
   .tweets
-    | {{ this.result_tweets }}
-
     | 検索欄
     .search
       label
         | query
       input(ref="query" type="text" value="s4na_penguin")
       label
-        | tart_datetime
-      input(ref="start_datetime" type="text" value="2019-11-15 23:59")
+        | start_datetime
+      input(ref="start_datetime" type="text" name="tweets-search[start_datetime]" value="2019-11-20 23:59")
       label
         | end_datetime
-      input(ref="end_datetime" type="text" value="2019-11-19 23:59")
+      input(ref="end_datetime" type="text" name="tweets-search[end_datetime]" value="2019-11-25 23:59")
       .search
-        button(@click="searchTweets")
+        button(type="button" @click="searchTweets")
           | 検索
 
-    .tweets_bodys
-      .tweets_body
+    .form__items
+      .form__item
         .search-tweets
           p
             | Tweets
           draggable(class="list-group" :list="result_tweets" group="people")
             .list-group-item(v-for="(element, index) in result_tweets" :key="element.name")
               tweet(:tweet="element")
-      .tweets_body
+      .form__item
         .note-tweets
           p
             | Notes
           draggable(class="list-group" :list="note_tweets" group="people")
             .list-group-item(v-for="(element, index) in note_tweets" :key="element.name")
               tweet(:tweet="element")
+      .form__item
+        .tweets_markdown
+          tweets_markdown(:value="note_tweets" title="List 1")
 
 </template>
 <script>
 import Draggable from 'vuedraggable'
 import Tweet from 'tweet'
+import TweetsMarkdown from 'tweets_markdown'
 
 export default {
   props: {},
   components: {
     'draggable': Draggable,
-    'tweet': Tweet
-  },
-  created() {
-    let ckeditor = document.createElement('script');
-    ckeditor.setAttribute('src',"//platform.twitter.com/widgets.js");
-    document.head.appendChild(ckeditor);
+    'tweet': Tweet,
+    'tweets_markdown': TweetsMarkdown,
   },
   data: function () {
     return {
       result_tweets: [],
       note_tweets: []
     }
+  },
+  created() {
+    this.note_tweets = JSON.parse(document.querySelector('#js-tweets-body').innerText);
   },
   methods: {
     token () {
@@ -86,11 +87,11 @@ export default {
 }
 </script>
 <style scoped lang="scss">
-.tweets_bodys {
+.form__items {
   display: flex;
   flex-direction: row;
 }
-.tweets_body {
-  max-width: 40%;
+.form__item {
+  width: 33%;
 }
 </style>
