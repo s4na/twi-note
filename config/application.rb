8c3446226947edbee8a492d0ae4d3723bbcb5771
s4na <appletea.umauma@gmail.com>
2019-12-13 16:43:36 +0900

    add loading locale file

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,9 @@ module TwiNote
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.i18n.load_path += Dir[Rails.root.join("config", "locales", "**", "*.{rb,yml}").to_s]
+    I18n.available_locales = [:ja]
     config.i18n.default_locale = :ja
   end
 end
