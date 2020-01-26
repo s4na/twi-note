6beed36b853d2be5de4933fae5099a53911b5f40
s4na <appletea.umauma@gmail.com>
2020-01-26 21:14:57 +0900

    Add full scratch CSS

--- app/javascript/tweet.vue
+++ app/javascript/tweet.vue
@@ -1,14 +1,13 @@
 <template lang="pug">
-  .cards__body
-    .cards__text.card(v-if="tweet['user'] == undefined")
-      .card-content
+  .card
+    template(v-if="tweet['user'] == undefined")
+      p
         | {{ tweet["markdown"] }}
-    .card(v-else class="twitter-tweet" data-lang="ja" data-dnt="true" data-theme="light").cards__text
-      .card-content
-        p(lang="ja" dir="ltr")
-          | {{ tweet["text"] }}
-          br
-          | &mdash; {{ tweet["user"]["name"] }} (@{{ tweet["user"]["screen_name"] }})
+    template(v-else class="twitter-tweet" data-lang="ja" data-dnt="true" data-theme="light")
+      p(lang="ja" dir="ltr")
+        | {{ tweet["text"] }}
+        br
+        | &mdash; {{ tweet["user"]["name"] }} (@{{ tweet["user"]["screen_name"] }})
 </template>
 <script>
 export default {
