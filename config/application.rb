3d8ce0161d2653f515663d9faf7ee8b4e8c39ccc
s4na <appletea.umauma@gmail.com>
2020-01-30 21:39:33 +0900

    change timezone to config/application.rb

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,8 @@ module TwiNote
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.time_zone = "Tokyo"
+    config.active_record.default_timezone = :local
 
     config.i18n.load_path += Dir[Rails.root.join("config", "locales", "**", "*.{rb,yml}").to_s]
     I18n.available_locales = [:ja, :en]
