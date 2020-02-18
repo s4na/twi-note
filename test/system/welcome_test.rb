f4508d1d9755f714e486080a08d11a791000673d
s4na <appletea.umauma@gmail.com>
2020-02-18 18:01:40 +0900

    fix welcome test

--- test/system/welcome_test.rb
+++ test/system/welcome_test.rb
@@ -11,6 +11,6 @@ class WelcomesTest < ApplicationSystemTestCase
 
   test "show welcome when user is not logged in" do
     visit root_path
-    assert_selector "h1", text: "勉強会のノートに、ツイートを貼ろう"
+    assert_selector "h1", text: "ノートにツイートを貼れるサービス"
   end
 end
