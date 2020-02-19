65e7d459502ee1b5e96510e97cd8dcde2a054b50
Masaki Komagata <komagata@gmail.com>
2020-02-19 17:02:57 +0900

    TwitterRepositoryのリファクタリング

--- app/controllers/api/tweets_controller.rb
+++ app/controllers/api/tweets_controller.rb
@@ -4,7 +4,7 @@ class Api::TweetsController < ApplicationController
   before_action :set_api_tweet, only: [:show, :edit, :update, :destroy]
 
   def index
-    tweet = TweetRepository.new
+    tweet = TweetRepository.new(ENV["TWITTWE_APP_ID"], ENV["TWITTWE_APP_SECRET"])
     @tweets = tweet.search(params)
   end
 end
---
+++ test/models/tweet_repository_test.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class TweetRepositoryTest < ActiveSupport::TestCase
+  test "#search" do
+  end
+end
