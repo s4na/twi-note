970ebb8e4d5cd2e8e564e5f3c7a6413e7326afda
s4na <appletea.umauma@gmail.com>
2020-02-21 13:27:33 +0900

    add prettier-eslint
    ## Reason
    - To add lint to Vue.js.

--- app/javascript/tweets.js
+++ app/javascript/tweets.js
@@ -4,25 +4,36 @@ import Tweets from './tweets.vue'
 document.addEventListener('DOMContentLoaded', () => {
   const tweets = document.getElementById('js-tweets')
   if (tweets) {
-    const noteTitleHumanAttributeName = tweets.getAttribute('data-note-title-human-attribute-name')
+    const noteTitleHumanAttributeName = tweets.getAttribute(
+      'data-note-title-human-attribute-name'
+    )
     const noteTitle = tweets.getAttribute('data-note-title')
     const noteEditMode = tweets.getAttribute('data-note-edit_mode')
-    const noteSearchSettingQuery = tweets.getAttribute('data-note-search_setting-query')
-    const noteSearchSettingStartDatetime = tweets.getAttribute('data-note-search_setting-start_datetime')
-    const noteSearchSettingEndDatetime = tweets.getAttribute('data-note-search_setting-end_datetime')
+    const noteSearchSettingQuery = tweets.getAttribute(
+      'data-note-search_setting-query'
+    )
+    const noteSearchSettingStartDatetime = tweets.getAttribute(
+      'data-note-search_setting-start_datetime'
+    )
+    const noteSearchSettingEndDatetime = tweets.getAttribute(
+      'data-note-search_setting-end_datetime'
+    )
     const datetimeCurrent = tweets.getAttribute('data-datetime-current')
 
     if (tweets) {
       new Vue({
-        render: h => h(Tweets, { props: {
-          noteTitleHumanAttributeName: noteTitleHumanAttributeName,
-          noteTitle: noteTitle,
-          noteEditMode: noteEditMode,
-          noteSearchSettingQuery: noteSearchSettingQuery,
-          noteSearchSettingStartDatetime: noteSearchSettingStartDatetime,
-          noteSearchSettingEndDatetime: noteSearchSettingEndDatetime,
-          datetimeCurrent: datetimeCurrent
-        } })
+        render: h =>
+          h(Tweets, {
+            props: {
+              noteTitleHumanAttributeName: noteTitleHumanAttributeName,
+              noteTitle: noteTitle,
+              noteEditMode: noteEditMode,
+              noteSearchSettingQuery: noteSearchSettingQuery,
+              noteSearchSettingStartDatetime: noteSearchSettingStartDatetime,
+              noteSearchSettingEndDatetime: noteSearchSettingEndDatetime,
+              datetimeCurrent: datetimeCurrent
+            }
+          })
       }).$mount('#js-tweets')
     }
   }
