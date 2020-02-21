92c0533c76399a1386e89d63725cbaf0d2bcb7b0
s4na <appletea.umauma@gmail.com>
2020-02-21 15:43:38 +0900

    clean TweetRepositoryTest

--- test/models/tweet_repository_test.rb
+++ test/models/tweet_repository_test.rb
@@ -6,52 +6,38 @@ require "webmock/minitest"
 class TweetRepositoryTest < ActiveSupport::TestCase
   test "#search" do
     # get oauth token
-    stub_request(:post, "https://api.twitter.com/oauth2/token").
-      with(
+    stub_request(:post, "https://api.twitter.com/oauth2/token")
+      .with(
         body: { "grant_type"=>"client_credentials" },
-        headers: {
-        "Accept"=>"*/*",
-        "Connection"=>"close",
-        "Content-Type"=>"application/x-www-form-urlencoded",
-        "Host"=>"api.twitter.com",
-        "User-Agent"=>"TwitterRubyGem/6.2.0"
-        }).
-      to_return(
+        headers: { "Connection"=>"close" }
+      )
+      .to_return(
         status: 200,
-        body: { "token_type": "bearer", "access_token": "AAAA%2FAAA%3DAAAAAAAA" }.to_json,
-        headers: {
-          'Content-Type': "application/json; charset=utf-8",
-        }
+        body: {}.to_json,
+        headers: { 'Content-Type': "application/json; charset=utf-8" }
       )
 
     # get twitter response
-    stub_request(:get, "https://api.twitter.com/1.1/search/tweets.json?count=100&exclude=retweets&q=hoge&result_type=recent&since_id&until=2020-02-19").
-    with(
-      headers: {
-      "Connection"=>"close",
-      "Host"=>"api.twitter.com",
-      "User-Agent"=>"TwitterRubyGem/6.2.0"
-      }).
-    to_return(
-      status: 200,
-      body:
-        {
-          statuses: [
-            {
-              created_at: "Tue Feb 18 23:59:57 +0000 2020",
-              id: 1229918506226864128,
-              id_str: "1229918506226864128",
-              text: "そろそろリモートワークやらフレックスやら活用していきたいゾ",
-              user: {
-                id_str: "3018621859",
-              },
-            }
-          ]
-        }.to_json,
-      headers: {
-        'Content-Type': "application/json; charset=utf-8",
-      }
-    )
+    stub_request(:get, "https://api.twitter.com/1.1/search/tweets.json?count=100&exclude=retweets&q=hoge&result_type=recent&since_id&until=2020-02-19")
+      .with(headers: { "Connection"=>"close" })
+      .to_return(
+        status: 200,
+        body:
+          {
+            statuses: [
+              {
+                created_at: "Tue Feb 18 23:59:57 +0000 2020",
+                id: 1229918506226864128,
+                id_str: "1229918506226864128",
+                text: "そろそろリモートワークやらフレックスやら活用していきたいゾ",
+                user: {
+                  id_str: "3018621859",
+                },
+              }
+            ]
+          }.to_json,
+        headers: { 'Content-Type': "application/json; charset=utf-8" }
+      )
 
     tweets = TweetRepository.new("id", "secret")
       .search(
