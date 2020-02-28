732145b16b3dc25c2cffc4c73e4de162d2349dc9
s4na <appletea.umauma@gmail.com>
2020-02-28 14:31:23 +0900

    change model test of user from block to mock_auth
    https://github.com/omniauth/omniauth/wiki/Integration-Testing

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,19 +4,16 @@ require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
   test ".find_for_twitter_oauth" do
-    auth = MiniTest::Mock.new
-    info = MiniTest::Mock.new.expect(:nickname, "Alice")
+    auth = OmniAuth::AuthHash.new({
+      provider: "Twitter",
+      uid: 1,
+      info: { nickname: "Alice" }
+    })
 
-    auth.expect :uid, 1 do
-      auth.expect :provider, "Twitter" do
-        auth.expect :info, info do
-          user = User.find_for_twitter_oauth(auth)
+    user = User.find_for_twitter_oauth(auth)
 
-          assert_equal "Alice", user.name
-          assert_equal "1", user.uid
-          assert_equal "Twitter", user.provider
-        end
-      end
-    end
+    assert_equal "Alice", user.name
+    assert_equal "1", user.uid
+    assert_equal "Twitter", user.provider
   end
 end
