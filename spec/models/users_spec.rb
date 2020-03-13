fe4982943d4861866060ba5898bc633333295fed
s4na <appletea.umauma@gmail.com>
2020-03-13 16:44:15 +0900

    fix test code like Minitest like RSpec

--- spec/models/users_spec.rb
+++ spec/models/users_spec.rb
@@ -4,18 +4,17 @@ require "rails_helper"
 
 RSpec.describe User, type: :model do
   describe ".find_for_twitter_oauth" do
-    it "" do
-      auth = OmniAuth::AuthHash.new({
+    let(:auth) {
+      OmniAuth::AuthHash.new({
         provider: "Twitter",
         uid: 1,
         info: { nickname: "Alice" }
       })
+    }
+    let(:user) { User.find_for_twitter_oauth(auth) }
 
-      user = User.find_for_twitter_oauth(auth)
-
-      expect("Alice").to eq user.name
-      expect("1").to eq user.uid
-      expect("Twitter").to eq user.provider
-    end
+    it { expect("Alice").to eq user.name }
+    it { expect("1").to eq user.uid }
+    it { expect("Twitter").to eq user.provider }
   end
 end
