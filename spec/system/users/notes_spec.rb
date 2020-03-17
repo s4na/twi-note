e5bed9f12c3167382360d289cf8c10058de82de8
s4na <appletea.umauma@gmail.com>
2020-03-17 18:39:52 +0900

    fix have_content typo to rspec

--- spec/system/users/notes_spec.rb
+++ spec/system/users/notes_spec.rb
@@ -5,16 +5,14 @@ require "rails_helper"
 describe "notes", type: :system do
   let!(:user_1) { FactoryBot.create(:user) }
   let!(:user_2) { FactoryBot.create(:user) }
-  let!(:note_2) { FactoryBot.create(:note, user: user_2, title: "user_2_note_title") }
+  let!(:note_2) { FactoryBot.create(:note, user: user_2) }
   let!(:search_setting_2) { FactoryBot.create(:search_setting, note: note_2) }
+  subject! { user_1.follow(user_2) }
 
-  before {
-    sign_in user_1
-    user_1.follow(user_2)
-  }
+  before { sign_in user_1 }
 
   context "show notes" do
     before { visit user_notes_path(user_2) }
-    it { expect(page).to have_content "user_2のノート一覧" }
+    it { expect(page).to have_content "User_2のノート一覧" }
   end
 end
