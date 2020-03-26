1f825c009cea1e9ef9a6471bb9d5b898447afe8d
s4na <appletea.umauma@gmail.com>
2020-03-26 14:44:51 +0900

    s

---
+++ spec/javascript/tweet.spec.js
@@ -0,0 +1,20 @@
+import { shallowMount } from '@vue/test-utils'
+import Tweet from '../../app/javascript/tweet.vue'
+
+describe('List.vue', () => {
+  it('renders li for each item in props.items', () => {
+    const items = ['1', '2']
+    const wrapper = shallowMount(Tweet, {
+      propsData: { items }
+    })
+    expect(wrapper.findAll('li')).toHaveLength(items.length)
+  })
+
+  // it('matches snapshot', () => {
+  //   const items = ['item 1', 'item 2']
+  //   const wrapper = shallowMount(List, {
+  //     propsData: { items }
+  //   })
+  //   expect(wrapper.html()).toMatchSnapshot()
+  // })
+})
