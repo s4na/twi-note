bdbf56b033d15de677d3249feb1aae883b2d6bd0
s4na <appletea.umauma@gmail.com>
2022-06-16 21:33:26 +0900

    Revert "./bin/rails app:update の実行結果"
    This reverts commit 85485e5b85d1120a006f2d21f0fc4ff02bad72d9.

--- config/application.rb
+++ config/application.rb
@@ -13,12 +13,28 @@ module TwiNote
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
+    config.time_zone = "Tokyo"
+    config.active_record.default_timezone = :local
+
+    config.i18n.load_path += Dir[Rails.root.join("config", "locales", "**", "*.{rb,yml}").to_s]
+    I18n.available_locales = [:ja, :en]
+    config.i18n.default_locale = :ja
+
+    config.to_prepare do
+      Devise::SessionsController.layout "welcome"
+    end
+
+    config.generators do |g|
+      g.test_framework :rspec,
+        controller_specs: false,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false,
+        request_specs: false
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,21 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "notes#index"
+  resources :privacy_policies, only: %i(index)
+  resources :disclaimers, only: %i(index)
+
+  namespace "api" do
+    resources :tweets, only: %i(index)
+  end
+
+  devise_for :users, only: [:sessions, :omniauth_callbacks], controllers: {
+    omniauth_callbacks: "users/omniauth_callbacks", only: %i(post)
+  }
+
+  namespace "notes" do
+    resources :download, only: %i(show)
+  end
+
+  resources :notes
 end
