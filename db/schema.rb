aa289590b05b6a8cd02b24def0694575a410f12d
s4na <appletea.umauma@gmail.com>
2022-07-05 00:59:01 +0900

    Rails updateからschemaがおかしかったので修正

--- db/schema.rb
+++ db/schema.rb
@@ -1,23 +1,21 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# This file is the source Rails uses to define your schema when running `rails
-# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_13_064805) do
+ActiveRecord::Schema[7.0].define(version: 2020_03_13_064805) do
   create_table "follows", force: :cascade do |t|
     t.integer "user_id"
     t.integer "followee_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["followee_id"], name: "index_follows_on_followee_id"
     t.index ["user_id", "followee_id"], name: "index_follows_on_user_id_and_followee_id", unique: true
     t.index ["user_id"], name: "index_follows_on_user_id"
@@ -29,8 +27,8 @@ ActiveRecord::Schema.define(version: 2020_03_13_064805) do
     t.text "tweets"
     t.text "all_search_result_tweets"
     t.integer "user_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string "edit_mode"
   end
 
@@ -39,8 +37,8 @@ ActiveRecord::Schema.define(version: 2020_03_13_064805) do
     t.string "query"
     t.string "start_datetime"
     t.string "end_datetime"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["note_id"], name: "index_search_settings_on_note_id"
   end
 
@@ -48,10 +46,10 @@ ActiveRecord::Schema.define(version: 2020_03_13_064805) do
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "reset_password_sent_at", precision: nil
+    t.datetime "remember_created_at", precision: nil
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string "provider"
     t.string "uid"
     t.string "name"
