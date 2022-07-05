8d7073281f6a8d16796d86f76984110369ea9698
s4na <appletea.umauma@gmail.com>
2022-07-06 00:08:28 +0900

    babel で @babel/plugin-proposal-private-property-in-object を無視するように設定

--- babel.config.js
+++ babel.config.js
@@ -66,7 +66,8 @@ module.exports = function (api) {
         {
           async: false
         }
-      ]
+      ],
+      ['@babel/plugin-proposal-private-property-in-object', { loose: true }]
     ].filter(Boolean)
   }
 }
